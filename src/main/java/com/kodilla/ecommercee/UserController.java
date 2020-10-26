2e2d63bcbedca61945df495a974a0e3e866ccbaa
AgaF <agnfal@gmail.com>
2020-10-26 18:44:59 +0100

    wstępna implementacja UserController

--- src/main/java/com/kodilla/ecommercee/UserController.java
+++ src/main/java/com/kodilla/ecommercee/UserController.java
@@ -1,4 +1,38 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/ecommercee/users")
 public class UserController {
+
+    @GetMapping(value = "getUsers")
+    public List<String> getUsers(){
+        return Arrays.asList("Adam Nowacki", "Stefan Kot", "Emilia Wąs");
+    }
+
+    @GetMapping(value = "getUser")
+    public String getUser(@RequestParam Long userId){
+        return "Adam Nowacki";
+    }
+
+    @PostMapping(value = "createUser", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void createUser(@RequestBody String newUser){
+        System.out.println("Create new user: " + newUser);
+    }
+
+    @PutMapping(value = "updateUser",consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void updateUser(@RequestParam Long userId, @RequestBody String updatedUserDetails){
+        System.out.println("Update user: " + userId + " new details: " + updatedUserDetails);
+    }
+
+    @DeleteMapping(value = "deleteUser")
+    public void deleteUser(@RequestParam Long userId){
+        System.out.println("Delete user: " + userId);
+    }
 }
+
