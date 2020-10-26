72cec4f4a7678fb7b2e8690107dde716c8818485
karolinakondziolka <karolina.kondziolka@gmail.com>
2020-10-26 23:36:32 +0100

    JDP201002-4 wstępna implementacja OrderController

--- src/main/java/com/kodilla/ecommercee/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/OrderController.java
@@ -1,4 +1,37 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/order")
 public class OrderController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "getOrders")
+    public List<OrderDto> getOrders() {
+        return new ArrayList();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getOrder")
+    public OrderDto getOrder(Long orderId) {
+        return new OrderDto(1L, "New order");
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createOrder")
+    public void OrderDto createOrder(OrderDto orderDto) {
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "updateOrder")
+    public OrderDto updateOrder(Long orderId) {
+        return new OrderDto(1L, "New updated order");
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteOrder")
+    public void deleteOrder(Long orderId) {
+    }
+
 }
