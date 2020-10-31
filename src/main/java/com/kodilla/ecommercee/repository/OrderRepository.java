d1c0e9715f1ee1e082c4a055af52d569558a5aaa
Agnieszka <agnfal@gmail.com>
2020-10-31 12:44:27 +0100

    dodanie OrderRepository

--- src/main/java/com/kodilla/ecommercee/repository/OrderRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/OrderRepository.java
@@ -1,4 +1,29 @@
 package com.kodilla.ecommercee.repository;
 
-public interface OrderRepository {
+import com.kodilla.ecommercee.domain.Order;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@Transactional
+public interface OrderRepository extends CrudRepository<Order,Long> {
+    @Override
+    List<Order> findAll();
+
+    @Override
+    Optional<Order> findById(Long id);
+
+    @Override
+    Order save(Order order);
+
+    @Override
+    void deleteById(Long id);
+
+    @Override
+    long count();
+
 }
