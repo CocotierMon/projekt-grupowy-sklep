d7d7d10786cccc5ae06a12c4f77074d418d74e39
CocotierMon <27955394+CocotierMon@users.noreply.github.com>
2020-11-10 23:42:17 +0100

    Dodanie repozytoriów

---
+++ src/main/java/com/kodilla/ecommercee/repository/DeliveryRepository.java
@@ -0,0 +1,36 @@
+package com.kodilla.ecommercee.repository;
+
+import com.kodilla.ecommercee.domain.Delivery;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@Transactional
+public interface DeliveryRepository extends CrudRepository<Delivery,Long> {
+    @Override
+    Delivery save(Delivery delivery);
+
+    @Override
+    Optional<Delivery> findById(Long id);
+
+    @Override
+    boolean existsById(Long id);
+
+    @Override
+    Iterable<Delivery> findAll();
+
+    @Override
+    long count();
+
+    @Override
+    void deleteById(Long id);
+
+    @Override
+    void delete(Delivery delivery);
+
+    @Override
+    void deleteAll();
+}
